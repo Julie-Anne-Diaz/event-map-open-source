44a428faf5da7feb4c47a9156c1c2bd7a0d44fb3
Julie-Anne-Diaz <jdiaz71126@gmail.com>
2026-09-20 18:53:23 -0400

    added to docker

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from routers import auth, events, users, friends, invites
 from database import Base, engine
 
@@ -9,7 +10,10 @@ app = FastAPI(title="Event Mapping API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://100.77.194.191:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,6 +24,7 @@ app.include_router(events.router)
 app.include_router(users.router)
 app.include_router(friends.router)
 app.include_router(invites.router)
+
 
 @app.get("/")
 def root():
